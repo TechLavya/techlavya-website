3efde494d7ccb121063e5afa6c9a6b668fd27c30
Rahul Roy <royrahulroy2004@gmail.com>
2025-03-18 22:38:44 +0530

    timeline events duration added

--- src/data/timelineData.ts
+++ src/data/timelineData.ts
@@ -12,39 +12,46 @@ export const timelineData: TimelineEntry[] = [
   {
     title: "Day 1",
     events: [
-      { title: "Hackvengers", type: "tech" },
-      { title: "0-Gravity", type: "tech" },
-      { title: "FREE FIRE MAX", type: "esports" },
-      { title: "BGMI", type: "esports" },
-      { title: "Rubik's Cube", type: "esports" },
+      { title: "Hackvengers ( 9am - 9pm )", type: "tech" },
+      { title: "Cad Builder ( 10am - 1pm )", type: "tech" },
+      { title: "Chess ( 2pm - 3pm )", type: "esports" },
+      { title: "Asphalt ( 4pm - 6pm )", type: "esports" },
+      { title: "BGMI ( 7pm - 9pm )", type: "esports" },
+
+
 
     ],
   },
   {
     title: "Day 2",
     events: [
-      { title: "Cad Builder", type: "tech" },
-      { title: "Roborush", type: "tech" },
-      { title: "eFootball", type: "esports" },
-      { title: "Mini Militia", type: "esports" },
+      { title: "School Event ( 8am - 4pm )", type: "tech" },
+      { title: "Seminar ( 10am - 12pm )", type: "tech" },
+      { title: "Acadexpo ( 2pm - 4pm )", type: "tech" },
+      { title: "FIFA (PC) ( 3pm - 5pm )", type: "esports" },
+      { title: "FREE FIRE MAX ( 6pm - 9pm )", type: "esports" },
+      { title: "Brainbuster ( 5pm - 7pm )", type: "tech" },
+
     ],
   },
   {
     title: "Day 3",
     events: [
-      { title: "Acadexpo", type: "tech" },
-      { title: "Circuit Design", type: "tech" },
-      { title: "Valorant (PC)", type: "esports" },
-      { title: "FIFA (PC)", type: "esports" },
+      { title: "0-Gravity ( 10am - 1pm )", type: "tech" },
+      { title: "Circuit Design ( 1pm - 4pm )", type: "tech" },
+      { title: "eFootball ( 5pm - 9pm )", type: "esports" },
+      { title: "Rubik's Cube ( 2pm - 3pm )", type: "esports" },
+      { title: "Valorant (PC) ( 4pm - 6pm )", type: "esports" },
     ],
   },
   {
     title: "Day 4",
     events: [
-      { title: "Brainbuster", type: "tech" },
-      { title: "Typing", type: "tech" },
-      { title: "Clash of Clans", type: "esports" },
-      { title: "Chess", type: "esports" },
+      { title: "Roborush ( 11am - 4pm )", type: "tech" },
+      { title: "Typing Test ( 12pm - 2pm )", type: "tech" },
+      { title: "Clash of Clans ( 2pm - 4pm )", type: "esports" },
+      { title: "Mini Militia ( 4pm - 5:30pm )", type: "esports" },
+
     ],
   },
 ];
