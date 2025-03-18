f8576e2bc2054aa4f40a71e73743a620bd82616f
Suresh1061 <sureshabantika@gmail.com>
2025-03-19 01:12:25 +0530

    update the counterdown data

--- src/data/countDown.ts
+++ src/data/countDown.ts
@@ -1,3 +1,3 @@
 // This file simply exports the target date you want to count down to.
 // Example: May 1, 2025, 12:00:00 UTC
-export const countdownTargetDate = new Date("2025-04-04T12:00:00Z");
+export const countdownTargetDate = new Date("2025-04-08T12:00:00Z");
