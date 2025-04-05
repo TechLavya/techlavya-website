2d0d6b014f61754ef30c6526944d918c984e487d
Suresh1061 <sureshabantika@gmail.com>
2025-04-05 10:44:23 +0530

    update the container style

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 const Container: React.FC<Props> = ({ children, id, title, props, titleClassName }) => {
      return (
-          <section id={id} className={` max-w-screen-xl w-full mx-auto max-md:h-auto min-h-screen px-4 relative ${title ? 'pt-20' : 'pt-28'}`} {...props}>
+          <section id={id} className={` max-w-screen-xl w-full mx-auto max-md:h-auto min-h-screen px-4 relative ${title ? 'pt-24' : 'pt-28'}`} {...props}>
                {title && <Title title={title} className={titleClassName} />}
                {children}
           </section>
