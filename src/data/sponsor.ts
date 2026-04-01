0402a9ebdfa8fc0def150659efa4baaa10fe64eb
Debojyoti <sahadebojyoti955@gmail.com>
2026-04-01 20:45:27 +0530

    Refactor code structure for improved readability and maintainability

--- src/data/sponsor.ts
+++ src/data/sponsor.ts
@@ -9,9 +9,13 @@ export const sponsorData: sponsorDataType[] = [
   { img: "/sponsor/ekchokho.png" },
   { img: "/sponsor/eventmas.png" },
   { img: "/sponsor/friends-fm.png" },
+  { img: "/sponsor/gfg.png" },
+  { img: "/sponsor/last%20moment.png" },
   { img: "/sponsor/pragati-cement.png" },
   { img: "/sponsor/reel2real.png" },
   { img: "/sponsor/sagar-raj-resorts.png" },
   { img: "/sponsor/sri-rajgharia.png" },
+  { img: "/sponsor/stockgrow.png" },
+  { img: "/sponsor/tending%20to%20infinity.png" },
   { img: "/sponsor/telegraph.png" },
 ];
